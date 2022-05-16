c7586c37687c8caf0ae036596ccc7acd49fb9329
vrezhMik <miqaelyan.vrej1999@gmail.com>
2022-05-16 23:17:09 +0400

    ft_printf

--- README.md
+++
@@ -1,6 +0,0 @@
-# ft_printf 🥵🥵🥵
-# Second project in the 42 School 🖥🖥🖥
-<h2> Reproducing behaviour of printf function </h2>
-To use tesetr read its Readme
-<br>
-No bonus included (100/125) ✅
--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,39 +6,23 @@
 /*   By: vmikayel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/18 11:24:52 by vmikayel          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2022/04/16 14:38:57 by vmikayel         ###   ########.fr       */
-=======
-/*   Updated: 2022/04/01 13:29:19 by vmikayel         ###   ########.fr       */
->>>>>>> 408d2e803f33384cb27f2db8ee2b59e39cfca6e6
+/*   Updated: 2022/05/16 23:13:52 by vmikayel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-<<<<<<< HEAD
 int	ft_atoi(const char *str)
 {
 	int	neg;
 	int	i;
 	int	num;
-=======
-int ft_atoi(const char *str)
-{
-	int neg;
-	int i;
-	int num;
->>>>>>> 408d2e803f33384cb27f2db8ee2b59e39cfca6e6
 
 	i = 0;
 	neg = 1;
 	num = 0;
-<<<<<<< HEAD
 	while (str[i] == ' ' || str[i] == '\n' || str[i] == '\t'
 		|| str[i] == '\v' || str[i] == '\f' || str[i] == '\r')
-=======
-	while (str[i] == ' ' || str[i] == '\n' || str[i] == '\t' || str[i] == '\v' || str[i] == '\f' || str[i] == '\r')
->>>>>>> 408d2e803f33384cb27f2db8ee2b59e39cfca6e6
 		i++;
 	if (str[i] == '-' || str[i] == '+')
 	{
